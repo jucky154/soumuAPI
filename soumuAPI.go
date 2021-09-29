fdb21f8f805bb0fd631ad14a485530c75402da2a
jucky154 <chemistry54@docomo.ne.jp>
2021-09-29 23:32:23 +0900

    fmt

--- soumuAPI.go
+++ soumuAPI.go
@@ -1,18 +1,15 @@
 package main
 
 import (
+	_ "embed"
 	"encoding/csv"
 	"encoding/json"
+	"errors"
+	"github.com/tadvi/winc"
 	"io/ioutil"
 	"net/http"
 	"strings"
-	"errors"
-	_ "embed"
-	"github.com/tadvi/winc"
 )
-
-
-
 
 // datファイルを読み込み
 //go:embed acag.dat
@@ -37,8 +34,8 @@ var stationview StationView
 type StationItem struct {
 	CallSign string
 	Location string
-	Number string
-	Power string
+	Number   string
+	Power    string
 }
 
 func (item StationItem) Text() (text []string) {
@@ -53,7 +50,7 @@ func (item StationItem) ImageIndex() int {
 	return 0
 }
 
-var numberTable  [][]string
+var numberTable [][]string
 
 // RadioSpec1からテーブルを生成
 // 形式: [型式, 周波数, 空中線電力] のリスト
@@ -74,13 +71,12 @@ func readACAG() {
 	numberTable, _ = numberReader.ReadAll()
 }
 
-
-func accessAPI() (*SearchResult, error){
+func accessAPI() (*SearchResult, error) {
 	//空データを作る
 	data := new(SearchResult)
 	//コールサインをzlogから取得
 	callSign := Query("$B")
-	if len(callSign) < 4{
+	if len(callSign) < 4 {
 		err := errors.New("callsign too short")
 		DisplayToast(err.Error())
 		return data, err
@@ -88,11 +84,11 @@ func accessAPI() (*SearchResult, error){
 
 	// APIからjsonデータを取得
 	url := "https://www.tele.soumu.go.jp/musen/list?ST=1&OF=2&DA=1&OW=AT&SK=2&DC=1&SC=1&MA=" + callSign
-	resp, err:= http.Get(url)
+	resp, err := http.Get(url)
 	defer resp.Body.Close()
 
 	//httpアクセスでエラーを吐いた時は出る
-	if  err != nil {
+	if err != nil {
 		DisplayToast(err.Error())
 		return data, err
 	}
@@ -108,7 +104,7 @@ func accessAPI() (*SearchResult, error){
 	return data, nil
 }
 
-func update(data SearchResult, frequency string){
+func update(data SearchResult, frequency string) {
 	//listを消す
 	stationview.list.DeleteAllItems()
 	// 検索にヒットした局ごとにコールサイン、JCC/JCGナンバーを出力
@@ -135,83 +131,83 @@ func update(data SearchResult, frequency string){
 		stationview.list.AddItem(StationItem{
 			CallSign: callSign,
 			Location: location,
-			Number: number,
-			Power: power,
+			Number:   number,
+			Power:    power,
 		})
 	}
 }
 
-func freqstring(index int) string{
-	switch{
-		case index == 0 : 
-			return "1910"
-		case index == 1 : 
-			return "3537.5"
-		case index == 2 : 
-			return "7100"
-		case index == 3 : 
-			return "10125"
-		case index == 4 : 
-			return "14175"
-		case index == 5 : 
-			return "18118"
-		case index == 6 : 
-			return "21225"
-		case index == 7 : 
-			return "24940"
-		case index == 8 : 
-			return "28.85"
-		case index == 9 : 
-			return "52"
-		case index == 10 : 
-			return "145"
-		case index == 11 : 
-			return "435"
-		case index == 12 : 
-			return "1280"
-		case index == 13 : 
-			return "2425"
-		case index == 14 : 
-			return "5750"
-		default:
-			return "1910"
+func freqstring(index int) string {
+	switch {
+	case index == 0:
+		return "1910"
+	case index == 1:
+		return "3537.5"
+	case index == 2:
+		return "7100"
+	case index == 3:
+		return "10125"
+	case index == 4:
+		return "14175"
+	case index == 5:
+		return "18118"
+	case index == 6:
+		return "21225"
+	case index == 7:
+		return "24940"
+	case index == 8:
+		return "28.85"
+	case index == 9:
+		return "52"
+	case index == 10:
+		return "145"
+	case index == 11:
+		return "435"
+	case index == 12:
+		return "1280"
+	case index == 13:
+		return "2425"
+	case index == 14:
+		return "5750"
+	default:
+		return "1910"
 	}
-}		
+}
 
-func makewindow(){
+func makewindow() {
 	// --- Make Window
 	mainWindow := winc.NewForm(nil)
 	mainWindow.SetSize(800, 600)
 	mainWindow.SetText("soumuAPI")
 
 	combo := winc.NewComboBox(mainWindow)
-	combo.InsertItem(0,"1.9MHz")
-	combo.InsertItem(1,"3.5MHz")
-	combo.InsertItem(2,"7MHz")
-	combo.InsertItem(3,"10MHz")
-	combo.InsertItem(4,"14MHz")
-	combo.InsertItem(5,"18MHz")
-	combo.InsertItem(6,"21MHz")
-	combo.InsertItem(7,"24MHz")
-	combo.InsertItem(8,"28MHz")
-	combo.InsertItem(9,"50MHz")
-	combo.InsertItem(10,"144MHz")
-	combo.InsertItem(11,"430MHz")
-	combo.InsertItem(12,"1.2GHz")
-	combo.InsertItem(13,"2.4GHz")
-	combo.InsertItem(14,"5.6GHz")
+	combo.InsertItem(0, "1.9MHz")
+	combo.InsertItem(1, "3.5MHz")
+	combo.InsertItem(2, "7MHz")
+	combo.InsertItem(3, "10MHz")
+	combo.InsertItem(4, "14MHz")
+	combo.InsertItem(5, "18MHz")
+	combo.InsertItem(6, "21MHz")
+	combo.InsertItem(7, "24MHz")
+	combo.InsertItem(8, "28MHz")
+	combo.InsertItem(9, "50MHz")
+	combo.InsertItem(10, "144MHz")
+	combo.InsertItem(11, "430MHz")
+	combo.InsertItem(12, "1.2GHz")
+	combo.InsertItem(13, "2.4GHz")
+	combo.InsertItem(14, "5.6GHz")
 	combo.SetSelectedItem(0)
-	
+
 	btn := winc.NewPushButton(mainWindow)
 	btn.SetText("check")
-	btn.SetPos(40,50)
-	btn.SetSize(100,40)
+	btn.SetPos(40, 50)
+	btn.SetSize(100, 40)
 
-	btn.OnClick().Bind(func(e *winc.Event){
+	btn.OnClick().Bind(func(e *winc.Event) {
 		index := combo.SelectedItem()
 		data, err := accessAPI()
 		freq := freqstring(index)
-		if err == nil{
+		if err == nil {
 			update(*data, freq)
 		}
 	})
